f1902c3b0379b8b01d4ff52cbad450fe9d63afe2
Evan Woods <efwoods@g.clemson.edu>
2018-03-10 17:38:08 -0500

    created unsorted file folder to distinguish against the A and a data

--- watson.py
+++ watson.py
@@ -29,16 +29,24 @@ print(json.dumps(classes, indent=2))
 
 prez.jpg
 fruitbowl.jpg
+
+
 ###
+Daniel
+5a4c8718738c1125628198f74376d5ac9d486df8
+
+###
+Evan
+988d558c4a7e45a98f2aa9f1d52a66d5be30287d
 '''
 import json
 from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3(
     '2016-05-20',
-    api_key='988d558c4a7e45a98f2aa9f1d52a66d5be30287d')
+    api_key='5a4c8718738c1125628198f74376d5ac9d486df8')
 
-with open('./prez.jpg', 'rb') as images_file:
+with open('./pictures/20180310_100553.jpg', 'rb') as images_file:
     faces = visual_recognition.classify(images_file) #parameters=json.dumps({'threshold':0.6}))
 print(json.dumps(faces, indent=2))
 
