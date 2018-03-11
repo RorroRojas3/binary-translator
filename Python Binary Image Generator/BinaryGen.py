9136d731b18810779712c98a42cb7b42b4cffc35
Daniel <dtoro@g.clemson.edu>
2018-03-11 04:51:06 -0400

    Splitter (Slider) added

---
+++ Python Binary Image Generator/BinaryGen.py
@@ -0,0 +1,90 @@
+from random import *
+import sys
+from PIL import Image
+import binascii
+def string2bits(s=''):
+    return [bin(ord(x))[2:].zfill(8) for x in s]
+
+def bits2string(b=None):
+    return ''.join([chr(int(x, 2)) for x in b])
+
+s = sys.argv[1]
+
+# b = string2bits(s)
+
+z = 0
+while(z < 30):
+
+    imageOrder = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    # print('String:')
+    # print(s)
+    #
+    # print('\nList of Bits:')
+    # for x in b:
+    #     print(x)
+    #
+    # print('\nString:')
+    # print(s2)
+    BinDigits = ['Space.jpg', 'Space.jpg', 'Space.jpg', 'Space.jpg', 'Space.jpg', 'Space.jpg', 'Space.jpg', 'Space.jpg']
+    n = 0
+
+
+    rand2 = randint(0, 7)
+    a = int(s)
+    if(a == 0):
+        BinDigits[rand2] = 'Zero.jpg'
+    if(a == 1):
+        BinDigits[rand2] = 'One.jpg'
+
+
+        n += 1
+    Spaces = ['', '', '', '', '','','','','']
+    Empty = 0
+    while(Empty < 9):
+        Spaces[Empty] = "Space.jpg"
+        Empty += 1
+
+
+    x = 0
+    images = map(Image.open, BinDigits)
+    # widths, heights = zip(*(i.size for i in images))
+    total_width = (430)*9
+    max_height = (430)*9
+
+    new_im = Image.new('RGB', (total_width, max_height))
+
+    Rand = randint(0, 8)
+    imageOrder[Rand] = 1
+    f = 1
+    for i in imageOrder:
+        if(i == 1):
+            if(a == 0):
+                BinDigits[rand2] = 'Zero.jpg'
+            if(a == 1):
+                BinDigits[rand2] = 'One.jpg'
+            images = map(Image.open, BinDigits)
+            x_offset = 0
+
+            y_offset = ((f)*400)
+            f+=1
+
+            for im in images:
+                new_im.paste(im, (x_offset,y_offset))
+                x_offset += 400
+        if(i == 0):
+            if(a == 0):
+                BinDigits[rand2] = 'Zero.jpg'
+            if(a == 1):
+                BinDigits[rand2] = 'One.jpg'
+            images = map(Image.open, Spaces)
+            x_offset = 0
+
+            y_offset = ((f)*400)
+            f+=1
+            for im in images:
+                new_im.paste(im, (x_offset,y_offset))
+                x_offset += 400
+    num = str(z)
+    new_im.save(s+num+'.jpg')
+    z += 1
